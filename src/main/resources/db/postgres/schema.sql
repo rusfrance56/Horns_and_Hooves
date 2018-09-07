718ef0fb2a998a74e7ed05527a343a374e4324a0
ruslan.husnutdinov <ruslan.husnutdinov@lamoda.ru>
2018-09-07 17:16:51 +0300

    change db script

--- src/main/resources/db/postgres/schema.sql
+++ src/main/resources/db/postgres/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS order_furniture;
+DROP TABLE IF EXISTS customer_order;
 DROP TABLE IF EXISTS employee_roles;
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS person;
 DROP TABLE IF EXISTS department;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -11,16 +11,40 @@ CREATE TABLE department (
   created       TIMESTAMP           DEFAULT now(),
   name          VARCHAR             NOT NULL
 );
-CREATE TABLE employee (
+CREATE TABLE person (
   id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   created       TIMESTAMP           DEFAULT now(),
   name          VARCHAR             NOT NULL,
-  department_id INTEGER             NOT NULL,
-  middle_name   VARCHAR             DEFAULT NULL,
-  sur_name      VARCHAR             NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+  surname       VARCHAR             NOT NULL,
+  middle_name   VARCHAR,
+  address       VARCHAR             NOT NULL,
+  phone         INTEGER             NOT NULL,
+  email         VARCHAR             NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department (id)
+--   ON DELETE CASCADE
 );
-CREATE TABLE order_furniture (
+CREATE TABLE customer_order (
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  created       TIMESTAMP           DEFAULT now(),
+  name          VARCHAR             NOT NULL,
+  due_date      TIMESTAMP           DEFAULT now(),
+  status        VARCHAR             NOT NULL,
+  priority      VARCHAR             NOT NULL,
+);
+CREATE TABLE item (
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  created       TIMESTAMP           DEFAULT now(),
+  name          VARCHAR             NOT NULL
+);
+CREATE TABLE order_items (
+  customer_order_id     INTEGER             NOT NULL,
+  item_id               VARCHAR,
+  CONSTRAINT order_items_idx UNIQUE (customer_order_id, item_id),
+  FOREIGN KEY (customer_order_id) REFERENCES customer_order (id),
+  FOREIGN KEY (item_id) REFERENCES item (id)
+);
+CREATE TABLE task (
   id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   created       TIMESTAMP           DEFAULT now(),
   name          VARCHAR             NOT NULL,
@@ -28,14 +52,20 @@ CREATE TABLE order_furniture (
   is_assigned   BOOL                DEFAULT FALSE,
   status        VARCHAR             NOT NULL,
   priority      VARCHAR             NOT NULL,
-  employee_id   INTEGER             DEFAULT NULL,
+  person_id     INTEGER,
   department_id INTEGER             NOT NULL,
-  FOREIGN KEY (employee_id) REFERENCES employee (id),
-  FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+  FOREIGN KEY (person_id) REFERENCES person (id),
+  FOREIGN KEY (department_id) REFERENCES department (id)
+--   ON DELETE CASCADE
 );
-CREATE TABLE employee_roles(
-  employee_id   INTEGER             NOT NULL,
+
+
+
+
+CREATE TABLE person_roles(
+  person_id     INTEGER             NOT NULL,
   role          VARCHAR,
-  CONSTRAINT employee_roles_idx UNIQUE (employee_id, role),
-  FOREIGN KEY (employee_id) REFERENCES employee (id) ON DELETE CASCADE
+  CONSTRAINT person_roles_idx UNIQUE (person_id, role),
+  FOREIGN KEY (person_id) REFERENCES person (id)
+--   ON DELETE CASCADE
 );
