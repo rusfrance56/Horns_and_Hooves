59f87cf67fae7ba7497b7ce34cf40e60c94dd975
ruslan.husnutdinov <ruslan.husnutdinov@lamoda.ru>
2019-01-30 10:26:27 +0300

    Added authorization begining

---
+++ src/main/java/com/rest_jpa/utils/SecurityUtil.java
@@ -0,0 +1,22 @@
+package com.rest_jpa.utils;
+
+import com.rest_jpa.entity.AppUser;
+import com.rest_jpa.servise.SocialUserDetailsImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.social.security.SocialUserDetails;
+
+import java.util.List;
+
+public class SecurityUtil {
+    // Auto Login.
+    public static void logInUser(AppUser user, List<String> roleNames) {
+
+        SocialUserDetails userDetails = new SocialUserDetailsImpl(user, roleNames);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
