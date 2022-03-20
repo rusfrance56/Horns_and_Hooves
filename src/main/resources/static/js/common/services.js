8935ff3fd522ab67fb5699fa9556fc5b1636c133
rusfrance <rusfrance56@gmail.com>
2022-03-20 18:59:52 +0300

    change location of front

--- src/main/resources/static/js/common/services.js
+++ src/main/resources/static/js/common/services.js
@@ -4,7 +4,7 @@ common.service('CommonService', function ($uibModal, $http) {
     this.openMessageModal = function (mode, message, modalClass) {
         var modal = $uibModal.open({
             animation: true,
-            templateUrl: '/js/common/views/messageModal.html',
+            templateUrl: '/static/js/common/views/messageModal.html',
             controller: 'messageModalCtrl',
             keyboard: false,
             backdrop: mode === 'success' ? true : 'static',
